0d9ae99f63e2eb49925e9b8251a90c4fb7ad6e47
Luna <yobapeka1@gmail.com>
2019-11-16 19:26:53 +0300

    Added dependecies for bot into venv\lib

---
+++ training_stuff/give_py_exercise.py
@@ -0,0 +1,54 @@
+# Parsing w3resource site with python exercises and outputing random exercise
+# Requests - HTTP reqeust lib
+# bs4 - HTML parser lib
+import requests
+from bs4 import BeautifulSoup
+# 1. get content from each exercise section left navbox <ul class="nav nav-list">
+reqs = requests.get('https://www.w3resource.com/python-exercises/')
+
+# https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
+#print(reqs)
+# https://en.wikipedia.org/wiki/List_of_HTTP_header_fields
+#print(reqs.headers)
+# Store page content.
+src = reqs.content
+# Start with bs4 parser
+# Create BeautifulSoup object based on src
+soup = BeautifulSoup(src, 'lxml')
+
+# find_all method to find all <ul> elements on the page with class "nav_list"
+navbox_list = soup.find_all("a")
+#print(navbox_list)
+
+# 2. From there, get a nav-header -> get <a href> link
+urls = []
+exr_urls = []
+for a_tag in navbox_list:
+    urls.append(a_tag.attrs['href'])
+
+for url in urls:
+    if "/python-exercises/" in url:
+        if "https://www.w3resource.com" not in url:
+            exr_urls.append("https://www.w3resource.com" + url)
+        else:
+            exr_urls.append(url)
+
+exercises = []
+for url_soup in exr_urls:
+    x = BeautifulSoup(requests.get(url_soup).content, 'lxml').find_all("p")
+    exercises.append(x)
+#надо выбрать <p> в нужном <div> брать все подряд нрн
+#пока подряд идущие <p> не закончатся
+#нечётный p - задание чётный - со ссылкой на ответ, вынимать её
+
+print(exr_urls)
+print(len(exr_urls))
+print(len(exercises))
+# 3. From each link get a <p> which has <strong>, which is header of exercise , do n+1
+# 4. From same <p> Else is text of this exercise
+# 5. create nested 2D list i,j
+# 6. header of exercise goes to i, rest is j
+# 7. after done with scraping(how do I know if it's done?), create random number x
+# 8. random number x is in range of 0:n, n - number of exercises
+# 9. Print list(x) and contents of it
+print('END')
--- venv/Scripts/pip3-script.py
+++ venv/Scripts/pip3-script.py
@@ -1,4 +1,4 @@
-#!C:\Users\xx\PycharmProjects\TelegaLogger\venv\Scripts\python.exe
+#!C:\Users\xx\PycharmProjects\Invalid-Python\venv\Scripts\python.exe
 # EASY-INSTALL-ENTRY-SCRIPT: 'pip==19.0.3','console_scripts','pip3'
 __requires__ = 'pip==19.0.3'
 import re
