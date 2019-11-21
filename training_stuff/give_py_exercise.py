07236abc7e4a8fe1a4cf0574ccc3c4893ffcb18b
Luna <yobapeka1@gmail.com>
2019-11-21 11:07:10 +0300

    edit

--- training_stuff/give_py_exercise.py
+++ training_stuff/give_py_exercise.py
@@ -1,9 +1,18 @@
 # Parsing w3resource site with python exercises and outputing random exercise
 # Requests - HTTP request lib
 # bs4 - HTML parser lib
+# 1. get content from each exercise section left navbox <ul class="nav nav-list">
+# 2. From there, get a nav-header -> get <a href> link
+# 3. From each link get a <p> which has <strong>, which is header of exercise , do n+1
+# 4. From same <p> Else is text of this exercise
+# 5. create nested 2D list i,j
+# 6. header of exercise goes to i, rest is j
+# 7. after done with scraping(how do I know if it's done?), create random number x
+# 8. random number x is in range of 0:n, n - number of exercises
+# 9. Print list(x) and contents of it
+
 import requests
 from bs4 import BeautifulSoup
-# 1. get content from each exercise section left navbox <ul class="nav nav-list">
 reqs = requests.get('https://www.w3resource.com/python-exercises/')
 
 # https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
@@ -20,7 +29,6 @@ soup = BeautifulSoup(src, 'lxml')
 navbox_list = soup.find_all("a")
 #print(navbox_list)
 
-# 2. From there, get a nav-header -> get <a href> link
 urls = []
 exr_urls = []
 for a_tag in navbox_list:
@@ -54,10 +62,3 @@ print(exercises)
 print('END')
 
 
-# 3. From each link get a <p> which has <strong>, which is header of exercise , do n+1
-# 4. From same <p> Else is text of this exercise
-# 5. create nested 2D list i,j
-# 6. header of exercise goes to i, rest is j
-# 7. after done with scraping(how do I know if it's done?), create random number x
-# 8. random number x is in range of 0:n, n - number of exercises
-# 9. Print list(x) and contents of it
