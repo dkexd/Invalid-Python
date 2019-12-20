16367cd94795738b1266b1977ec1219567205f9b
Luna <yobapeka1@gmail.com>
2019-12-20 22:30:25 +0300

    Added file sorting by extensions

---
+++ training_stuff/sorting_your_shitpile.py
@@ -0,0 +1,35 @@
+#  Sorts files in programs dir by extensions(.exe, .pdf, ...) to sorted dir
+
+
+import os
+import shutil
+
+
+def sort_files():
+    root_path = os.getcwd()
+    root_files = os.listdir(root_path)
+    #  check for sorted folder
+    if 'sorted' not in root_files:
+        os.makedirs('sorted')
+    os.chdir(root_path + "\\sorted\\")
+    sorted_path = os.getcwd()
+    sorted_files = os.listdir(sorted_path)
+    file_ext = file.split('.')
+    for file in root_files:
+        print("file: " + file)
+        print("file_ext: " + str(file_ext))
+        #  check that we have a file with ext and not our script
+        if len(file_ext) > 1 and file != 'sorting_your_shitpile.py':
+            #  check that we create new subfolder
+            if file_ext[-1] not in sorted_files:
+                os.makedirs(file_ext[-1])
+                print('Made directory: ' + str(file_ext[1]))
+            # check that we're not  moving same file
+            # !!! improve it to differentiate versions of the same file (by size?)
+            if file not in os.listdir(sorted_path + '\\' + str(file_ext[-1])):
+                shutil.move(root_path + '\\' + file, sorted_path + '\\' + str(file_ext[1]))
+                print('Moved ' + file)
+        # shutil.move(file, new_dir)
+
+
+sort_files()
