869ed1ba56aaa705e911f12b9085198701469dc8
Luna <yobapeka1@gmail.com>
2020-01-04 17:56:59 +0300

    Fixed error occured with files containing multiple "." in the filename (f.ex. BsgLauncher.0.9.1.935.exe)

--- training_stuff/sorting_your_shitpile.py
+++ training_stuff/sorting_your_shitpile.py
@@ -1,5 +1,6 @@
 #  Sorts files in programs dir by extensions(.exe, .pdf, ...) to sorted dir
-
+#  IMPROVE?
+#  1. to differentiate versions of the same file
 
 import os
 import shutil
@@ -14,20 +15,21 @@ def sort_files():
     os.chdir(root_path + "\\sorted\\")
     sorted_path = os.getcwd()
     sorted_files = os.listdir(sorted_path)
-    file_ext = file.split('.')
     for file in root_files:
+        file_ext = file.split('.')
         print("file: " + file)
         print("file_ext: " + str(file_ext))
+        print("file_ext[-1]: " + file_ext[-1])
         #  check that we have a file with ext and not our script
         if len(file_ext) > 1 and file != 'sorting_your_shitpile.py':
             #  check that we create new subfolder
             if file_ext[-1] not in sorted_files:
                 os.makedirs(file_ext[-1])
-                print('Made directory: ' + str(file_ext[1]))
+                print('Made directory: ' + str(file_ext[-1]))
             # check that we're not  moving same file
-            # !!! improve it to differentiate versions of the same file (by size?)
+            # !!! improve it to differentiate versions of the same file
             if file not in os.listdir(sorted_path + '\\' + str(file_ext[-1])):
-                shutil.move(root_path + '\\' + file, sorted_path + '\\' + str(file_ext[1]))
+                shutil.move(root_path + '\\' + file, sorted_path + '\\' + str(file_ext[-1]))
                 print('Moved ' + file)
         # shutil.move(file, new_dir)
 
